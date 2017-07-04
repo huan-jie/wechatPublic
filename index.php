3950fca88bb48612e784738e57ef31e12ce82de0
huan-jie <nahuanjie@gmail.com>
2017-07-04 14:08:28 +0800

    4

--- index.php
+++ index.php
@@ -35,74 +35,9 @@ if (isset($_GET['echostr'])) {
 		$reciveMessageObj->judgeMessageType();
 
 		// 实例回复用户消息类
-		$responseMessageObj = new responseMessageClass($reciveMessageObj->toUserName, $reciveMessageObj->fromUserName, $reciveMessageObj->msgType, $reciveMessageObj->resultContent);
+		$responseMessageClassgeObj = new responseMessageClass($reciveMessageObj->toUserName, $reciveMessageObj->fromUserName, $reciveMessageObj->msgType, $reciveMessageObj->resultContent);
 
-		// 对不同的事件做自定义处理
-		switch ($reciveMessageObj->msgType) {
-			case 'text':
-				// 文字
-				$responseMessageObj->responseText('hello world');
-				break;
-			case 'image':
-				// 图片
-				
-				break;
-			case 'voice':
-				// 语音
-				
-				break;
-			case 'video':
-				// 视频
-				
-				break;
-			case 'shortvideo':
-				// 小视频		
-				
-				break;
-			case 'location':
-				// 位置
-
-				break;
-			case 'link':
-				// 链接
-				
-				break;
-			case 'event':
-				// 事件
-				switch ($event) {
-					case 'subscribe':
-						// 关注
-
-						break;
-					case 'unsubscribe':
-						// 取消关注
-					
-						break;
-					case 'SCAN':
-						// 扫描
-					
-						break;
-					case 'LOCATION':
-						// 上报地理位置
-					
-						break;
-					case 'CLICK':
-						// 点击菜单拉取消息
-					
-						break;
-					case 'VIEW':
-						// 点击菜单跳转链接
-					
-						break;
-					default:
-						# code...
-						break;
-				}
-				break;
-			default:
-				echo "{'error':'error type'}"
-				break;
-		}
+		
 	} else {
 
 		echo "empty data";
