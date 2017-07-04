3d21912f563d02bf79dcb260872683a148549bdf
huan-jie <nahuanjie@gmail.com>
2017-07-04 14:05:01 +0800

    2

--- index.php
+++ index.php
@@ -20,92 +20,92 @@ if (isset($_GET['echostr'])) {
 		echo $echostr;
 		exit;
 	}
-} // else {
+} else {
 
-// 	// 接收消息，微信服务器会post xml类型的数据过来
-// 	$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+	// 接收消息，微信服务器会post xml类型的数据过来
+	$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-// 	if (!empty($postStr)) {
+	if (!empty($postStr)) {
 
-// 		include("reciveMessageClass.php");
-// 		include("responseMessageClass.php");
+	// 	include("reciveMessageClass.php");
+	// 	include("responseMessageClass.php");
 
-// 		// 解析用户发送的内容
-// 		$reciveMessageObj = new reciveMessageClass($postStr);
-// 		$reciveMessageObj->judgeMessageType();
+	// 	// 解析用户发送的内容
+	// 	$reciveMessageObj = new reciveMessageClass($postStr);
+	// 	$reciveMessageObj->judgeMessageType();
 
-// 		// 实例回复用户消息类
-// 		$responseMessageObj = new responseMessageClass($reciveMessageObj->toUserName, $reciveMessageObj->fromUserName, $reciveMessageObj->msgType, $reciveMessageObj->resultContent);
+	// 	// 实例回复用户消息类
+	// 	$responseMessageObj = new responseMessageClass($reciveMessageObj->toUserName, $reciveMessageObj->fromUserName, $reciveMessageObj->msgType, $reciveMessageObj->resultContent);
 
-// 		// 对不同的事件做自定义处理
-// 		switch ($reciveMessageObj->msgType) {
-// 			case 'text':
-// 				// 文字
-// 				$responseMessageObj->responseText('hello world');
-// 				break;
-// 			case 'image':
-// 				// 图片
+	// 	// 对不同的事件做自定义处理
+	// 	switch ($reciveMessageObj->msgType) {
+	// 		case 'text':
+	// 			// 文字
+	// 			$responseMessageObj->responseText('hello world');
+	// 			break;
+	// 		case 'image':
+	// 			// 图片
 				
-// 				break;
-// 			case 'voice':
-// 				// 语音
+	// 			break;
+	// 		case 'voice':
+	// 			// 语音
 				
-// 				break;
-// 			case 'video':
-// 				// 视频
+	// 			break;
+	// 		case 'video':
+	// 			// 视频
 				
-// 				break;
-// 			case 'shortvideo':
-// 				// 小视频		
+	// 			break;
+	// 		case 'shortvideo':
+	// 			// 小视频		
 				
-// 				break;
-// 			case 'location':
-// 				// 位置
+	// 			break;
+	// 		case 'location':
+	// 			// 位置
 
-// 				break;
-// 			case 'link':
-// 				// 链接
+	// 			break;
+	// 		case 'link':
+	// 			// 链接
 				
-// 				break;
-// 			case 'event':
-// 				// 事件
-// 				switch ($event) {
-// 					case 'subscribe':
-// 						// 关注
+	// 			break;
+	// 		case 'event':
+	// 			// 事件
+	// 			switch ($event) {
+	// 				case 'subscribe':
+	// 					// 关注
 
-// 						break;
-// 					case 'unsubscribe':
-// 						// 取消关注
+	// 					break;
+	// 				case 'unsubscribe':
+	// 					// 取消关注
 					
-// 						break;
-// 					case 'SCAN':
-// 						// 扫描
+	// 					break;
+	// 				case 'SCAN':
+	// 					// 扫描
 					
-// 						break;
-// 					case 'LOCATION':
-// 						// 上报地理位置
+	// 					break;
+	// 				case 'LOCATION':
+	// 					// 上报地理位置
 					
-// 						break;
-// 					case 'CLICK':
-// 						// 点击菜单拉取消息
+	// 					break;
+	// 				case 'CLICK':
+	// 					// 点击菜单拉取消息
 					
-// 						break;
-// 					case 'VIEW':
-// 						// 点击菜单跳转链接
+	// 					break;
+	// 				case 'VIEW':
+	// 					// 点击菜单跳转链接
 					
-// 						break;
-// 					default:
-// 						# code...
-// 						break;
-// 				}
-// 				break;
-// 			default:
-// 				echo "{'error':'error type'}"
-// 				break;
-// 		}
-// 	} else {
+	// 					break;
+	// 				default:
+	// 					# code...
+	// 					break;
+	// 			}
+	// 			break;
+	// 		default:
+	// 			echo "{'error':'error type'}"
+	// 			break;
+	// 	}
+	} else {
 
-// 		echo "empty data";
-// 	}
+		echo "empty data";
+	}
 
-// }
+}
