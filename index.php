7a905d6edfeb2a01a1ff022467c3afcedc7a79c8
huan-jie <nahuanjie@gmail.com>
2017-07-04 14:33:39 +0800

    11

--- index.php
+++ index.php
@@ -11,44 +11,44 @@ require("./responseMessageClass.php");
 // define custom token
 define("TOKEN", "nahuanjie");
 
-if (isset($_GET['echostr'])) {
+// if (isset($_GET['echostr'])) {
 	
-	// 验证签名
-	$signatureObj = new signatureClass();
-	$echostr = $_GET['echostr'];
+// 	// 验证签名
+// 	$signatureObj = new signatureClass();
+// 	$echostr = $_GET['echostr'];
 	
-	if ($signatureObj->checkSignature(TOKEN, $_GET['timestamp'], $_GET['nonce'], $_GET['signature'])) {
+// 	if ($signatureObj->checkSignature(TOKEN, $_GET['timestamp'], $_GET['nonce'], $_GET['signature'])) {
 		
-		echo $echostr;
-		exit;
-	}
-} else {
+// 		echo $echostr;
+// 		exit;
+// 	}
+// } else {
 
-	// 接收消息，微信服务器会post xml类型的数据过来
-	$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+// 	// 接收消息，微信服务器会post xml类型的数据过来
+// 	$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-	if (!empty($postStr)) {
+// 	if (!empty($postStr)) {
 
-		// 解析用户发送的内容
-		$reciveMessageObj = new reciveMessageClass($postStr);
-		$reciveMessageObj->judgeMessageType();
+// 		// 解析用户发送的内容
+// 		$reciveMessageObj = new reciveMessageClass($postStr);
+// 		$reciveMessageObj->judgeMessageType();
 
-		// 实例回复用户消息类
-		$responseMessageObj = new responseMessageClass($reciveMessageObj->toUserName, $reciveMessageObj->fromUserName, $reciveMessageObj->msgType, $reciveMessageObj->resultContent);
+// 		// 实例回复用户消息类
+// 		$responseMessageObj = new responseMessageClass($reciveMessageObj->toUserName, $reciveMessageObj->fromUserName, $reciveMessageObj->msgType, $reciveMessageObj->resultContent);
 
-		// 对不同的事件做自定义处理
-		switch ($reciveMessageObj->msgType) {
-			case 'text':
-				// 文字
-				$responseMessageObj->responseText('hello world');
-				break;
+// 		// 对不同的事件做自定义处理
+// 		switch ($reciveMessageObj->msgType) {
+// 			case 'text':
+// 				// 文字
+// 				$responseMessageObj->responseText('hello world');
+// 				break;
 			
-			default:
-				break;
-		}
-	} else {
+// 			default:
+// 				break;
+// 		}
+// 	} else {
 
-		echo "empty data";
-	}
+// 		echo "empty data";
+// 	}
 
-}
+// }
